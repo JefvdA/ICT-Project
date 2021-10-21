d2d864b0bbc06bd424a72a10574599bda64fdc7a
Mohammed Souleymane <mohammedsouleymane@outlook.com>
2021-10-21 21:53:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const multer = require('multer');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 path = require("path");
+const fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, 'Uploads')
     },
     filename: function(req, file, cb) {
-        cb(null, file.originalname)
+        cb(null, file.originalname.replaceAll(" ", "_"))
     }
 })
 
@@ -23,7 +24,14 @@ app.get('/', (req, res) => {
 
 app.get('/api/files/:uuid', (req, res) => {
 
-    res.download(path.join(__dirname, "Uploads/" + req.params.uuid));
+    //checksum
+    var dateCreated = fs.statSync(path.join(__dirname, "Uploads/" + req.params.uuid)).birthtime;
+    if (((new Date().getTime() - dateCreated.getTime()) / 60000) > 1440)
+        res.send("24 hours have passed");
+    else
+        res.download(path.join(__dirname, "Uploads/" + req.params.uuid));
+    console.log((new Date().getTime() - dateCreated.getTime()) / 60000)
+
 })
 
 app.post('/api/files', upload.single('myFile'), (req, res, next) => {
@@ -33,7 +41,7 @@ app.post('/api/files', upload.single('myFile'), (req, res, next) => {
         error.httpStatusCode = 400
         return next(error)
     }
-    res.send(file)
+    res.send(file.originalname.replaceAll(" ", "_") + " has been uploaded")
 
 })
 app.listen(3000, () => console.log('Server ready'))
