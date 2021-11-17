a2b0117a3ea7a62faceb9b64e893abe3604a55b8
Mohammed Souleymane <Mohammedsouleymane@outlook.com>
2021-11-17 19:04:15 +0100

    login

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import * as path from 'path'
 import * as fs from 'fs'
 
 import { DownloadFile, UploadFile } from './AWS/s3Functions.js'
-
+import * as security from './security.js'
 import express from 'express'
 import fileUpload from 'express-fileupload'
+const __dirname = path.resolve()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -12,13 +13,25 @@ app.use(fileUpload())
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(path.resolve(), "upload.html"))
+    res.sendFile(path.join(__dirname, "index.html"))
 })
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "login.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "register.html"))
+})
+app.get('/upload', (req, res) => {
+
+    res.sendFile(path.join(__dirname, "upload.html"))
+})
+
 
 app.get('/api/files/:uuid', (req, res) => {
     const uuid = req.params.uuid
 
-    DownloadFile(uuid).then((fileStream) =>{
+    DownloadFile(uuid).then((fileStream) => {
         res.attachment(uuid.split(":")[1]) // Get filename through UUID parameter -> Later replace this with filename gotten out of rds database
         fileStream.pipe(res)
     })
@@ -36,4 +49,21 @@ app.post('/api/files', (req, res, next) => {
     res.send(output)
 })
 
+app.post('/api/register', (req, res) => {
+    res.send(security.registerUser(req.body.myEmail, req.body.myPassword))
+})
+
+app.post('/api/login', (req, res) => {
+
+    let output = security.login(req.body.myEmail, req.body.myPassword)
+    output.then(
+        function(value) {
+            res.sendFile(path.join(__dirname, "upload.html"))
+        },
+        function(error) {
+            console.log(error)
+            res.redirect(req.get('referer'));
+        }
+    );
+})
 app.listen(3000, () => console.log('Server ready'))
